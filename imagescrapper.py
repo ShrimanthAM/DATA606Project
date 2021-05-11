87174391b1411994cbab509bc31269b1116b7815
Shrimanth <shrimanth.m@gmail.com>
2021-05-11 09:39:22 -0400

    Update imagescrapper.py

--- imagescrapper.py
+++ imagescrapper.py
@@ -1,3 +1,4 @@
+#importing the required libraries
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,26 +10,28 @@ import os
 from PIL import Image
 import hashlib
 
-## my changes 
+ 
 # Chromedriver path
 DRIVER_PATH = 'C:\\Users\Shrimanth\Desktop\Scrapping\chromedriver_win32\chromedriver'
 
-def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_between_interactions:int=1):
-    def scroll_to_end(wd):
+#function to fetch links of the image search 
+def fetch_image_urls(search_query:str, max_fetch:int, wd:webdriver, sleep_time:int=1):
+    
+    def go_to_end(wd):
         wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(sleep_between_interactions)
+        time.sleep(sleep_time)
 
-    # Build the Google query
+    # Build the Google search query 
     search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
 
     # Load the page
-    wd.get(search_url.format(q=query))
+    wd.get(search_url.format(q=search_query))
 
     image_urls = set()
     image_count = 0
     results_start = 0
-    while image_count < max_links_to_fetch:
-        scroll_to_end(wd)
+    while image_count < max_fetch:
+        go_to_end(wd)
 
         # Get all image thumbnail results
         thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
@@ -37,14 +40,14 @@ def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_betw
         print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
 
         for img in thumbnail_results[results_start:number_results]:
-            # Try to click every thumbnail such that we can get the real image behind it
+            # clicking every thumbnail images
             try:
                 img.click()
-                time.sleep(sleep_between_interactions)
+                time.sleep(sleep_time)
             except Exception:
                 continue
 
-            # Extract image URLs
+            # Extract image links
             actual_images = wd.find_elements_by_css_selector('img.n3VNCb')
             for actual_image in actual_images:
                 if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
@@ -52,7 +55,7 @@ def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_betw
 
             image_count = len(image_urls)
 
-            if len(image_urls) >= max_links_to_fetch:
+            if len(image_urls) >= max_fetch:
                 print(f"Found: {len(image_urls)} image links, done!")
                 break
         else:
@@ -61,9 +64,9 @@ def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_betw
             return
             load_more_button = wd.find_element_by_css_selector(".mye4qd")
             if load_more_button:
-                wd.execute_script("document.querySelector('.mye4qd').click();")
+                wd.execute_script("document.search_querySelector('.mye4qd').click();")
 
-        # Move the result startpoint further down
+        # Moving the result search down 
         results_start = len(thumbnail_results)
 
     return image_urls
@@ -85,25 +88,21 @@ def persist_image(folder_path:str, url:str):
     except Exception as e:
         print(f"ERROR - Could not save {url} - {e}")
 
-# As soon as the number of images is over 80, an error is shown
+
 def search_and_download(search_term:str, target_path='/Users/Shrimanth/GoogleImages', number_images=70):
     target_folder = os.path.join(target_path, '_'.join(search_term.lower().split(' ')))
 
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
 
-    #with webdriver.Chrome() as wd:  my changes 
+    #with webdriver.Chrome() as wd:
     with webdriver.Chrome(executable_path=DRIVER_PATH) as wd:
-        res = fetch_image_urls(search_term, number_images, wd=wd, sleep_between_interactions=0.5) #or [] # my changes
+        res = fetch_image_urls(search_term, number_images, wd=wd, sleep_time=0.5) #or [] # my changes
  
-    #if res: # my changes
     for elem in res:
         persist_image(target_folder, elem)
-    #for elem in res:
-   #     persist_image(target_folder, elem)
-
-# Change here to modify the search query
-
+    
+# my search string 
 search_term = 'Amazon'
 
 search_and_download(
